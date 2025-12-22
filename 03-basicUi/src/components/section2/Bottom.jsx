08c6d75b3f4854d291a7cef506f79335d6666bff
Subham3008 <subhamsamanta065@gmail.com>
2025-12-23 01:19:03 +0530

    4th commit

--- 03-basicUi/src/components/section2/Bottom.jsx
+++ 03-basicUi/src/components/section2/Bottom.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Bottom = () => {
   return (
-    <div>
-      
+    <div className="bottom-content">
+      <div className="content">
+        <h1>12 000+</h1>
+        <p>Hours of play annually</p>
+      </div>
+      <div className="content">
+        <h1>89%</h1>
+        <p>Player Retention Rate</p>
+      </div>
+      <div className="content">
+        <h1>1,200+</h1>
+        <p>Active Members</p>
+      </div>
+      <div className="content">
+        <h1>125+</h1>
+        <p>Annual Tournaments</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Bottom
+export default Bottom;
--- 03-basicUi/src/components/section2/Middle.jsx
+++ 03-basicUi/src/components/section2/Middle.jsx
@@ -3,9 +3,34 @@ import React from "react";
 const Middle = () => {
   return (
     <div className="mid">
-      <div className="left elem"></div>
-      <div className="mid elem"></div>
-      <div className="right elem"></div>
+      <div className="left elem">
+        <h3>
+          Proffessional hard courts{" "}
+          <span>
+            with tournament-grade lighting & climate control - play in
+          </span>{" "}
+          perfect conditions in any sesson.
+        </h3>
+        <h4>Game Mode</h4>
+      </div>
+      <div className="cen elem">
+        <h3>Private & Group Lessons</h3>
+      </div>
+      <div className="right elem">
+        <div className="top">
+          <h1>100+</h1>
+          <h3>Pro Coaches</h3>
+          <p>
+            Certified professionals reafy to boost your <br />from first serve to
+            tournament level.
+          </p>
+        </div>
+        <div className="bottom">
+            <h4>Beginner</h4>
+            <h4>Intermediate</h4>
+            <h4>Adanced</h4>
+        </div>
+      </div>
     </div>
   );
 };
