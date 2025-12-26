ed7c4a1f600de5fb9439ff6ae8cc0fdecff73b81
Subham3008 <subhamsamanta065@gmail.com>
2025-12-26 19:36:31 +0530

    3rd commit

--- 04-props/src/App.jsx
+++ 04-props/src/App.jsx
@@ -6,17 +6,17 @@ const App = () => {
     {
       name: "Iron Man",
       image:
-        "https://cdn.dribbble.com/users/1162077/screenshots/3848914/ironman.png",
-      likes: 1200,
+        "https://i.pinimg.com/736x/d8/9d/ee/d89deed5b149094e9f97a02a6fc19764.jpg",
+      likes: "12.5k",
       post: 45,
       views: 25000,
-      description: "Genius, billionaire, playboy, philanthropist.",
+      description: "Genius, billionaire, philanthropist.",
     },
     {
       name: "Captain America",
       image:
-        "https://cdn.dribbble.com/users/113499/screenshots/1538734/captain-america.png",
-      likes: 980,
+        "https://i.pinimg.com/736x/64/68/1c/64681c27076f1163c2e50c68de351c5d.jpg",
+      likes: "9k",
       post: 38,
       views: 21000,
       description: "The first Avenger.",
@@ -24,8 +24,8 @@ const App = () => {
     {
       name: "Thor",
       image:
-        "https://cdn.dribbble.com/users/1022485/screenshots/2758039/thor.png",
-      likes: 1500,
+        "https://i.pinimg.com/736x/70/14/e0/7014e01471b17ccd33fe2dd4d195c776.jpg",
+      likes: "15.2k",
       post: 50,
       views: 30000,
       description: "God of Thunder.",
@@ -33,8 +33,8 @@ const App = () => {
     {
       name: "Hulk",
       image:
-        "https://cdn.dribbble.com/users/119254/screenshots/1462451/hulk.png",
-      likes: 1100,
+        "https://i.pinimg.com/736x/38/5d/c7/385dc7243e7d637d54a1be7ea216bb43.jpg",
+      likes: "11.5k",
       post: 32,
       views: 22000,
       description: "Smash everything.",
@@ -42,8 +42,8 @@ const App = () => {
     {
       name: "Spider-Man",
       image:
-        "https://cdn.dribbble.com/users/1787323/screenshots/3897368/spiderman.png",
-      likes: 2000,
+        "https://i.pinimg.com/736x/dc/ff/cb/dcffcb5d8c5080275e83796f264e35c6.jpg",
+      likes: "30k",
       post: 60,
       views: 40000,
       description: "Friendly neighborhood hero.",
@@ -51,8 +51,8 @@ const App = () => {
     {
       name: "Batman",
       image:
-        "https://cdn.dribbble.com/users/1186261/screenshots/3718681/batman.png",
-      likes: 1750,
+        "https://i.pinimg.com/736x/ce/05/64/ce0564c18feb9344f0d0636498214beb.jpg",
+      likes: "20k",
       post: 55,
       views: 35000,
       description: "The Dark Knight.",
@@ -60,8 +60,8 @@ const App = () => {
     {
       name: "Superman",
       image:
-        "https://cdn.dribbble.com/users/1751799/screenshots/3686077/superman.png",
-      likes: 1600,
+        "https://i.pinimg.com/736x/09/ac/e3/09ace37fb8460ded9b4f186f4890b594.jpg",
+      likes: "16.2k",
       post: 48,
       views: 33000,
       description: "Man of Steel.",
@@ -69,8 +69,8 @@ const App = () => {
     {
       name: "Wonder Woman",
       image:
-        "https://cdn.dribbble.com/users/143058/screenshots/1449443/wonder_woman.png",
-      likes: 1400,
+        "https://i.pinimg.com/1200x/29/bd/52/29bd522f97d4a609af59d500085a0f5f.jpg",
+      likes: "14k",
       post: 42,
       views: 28000,
       description: "Amazon warrior.",
@@ -78,8 +78,8 @@ const App = () => {
     {
       name: "Black Panther",
       image:
-        "https://cdn.dribbble.com/users/181704/screenshots/2552197/black-panther.png",
-      likes: 1300,
+        "https://i.pinimg.com/1200x/42/00/a8/4200a84e5de842e1bcd36ce95a05f57d.jpg",
+      likes: "9k",
       post: 40,
       views: 26000,
       description: "King of Wakanda.",
@@ -87,7 +87,7 @@ const App = () => {
     {
       name: "Flash",
       image:
-        "https://cdn.dribbble.com/users/1126935/screenshots/2814427/flash.png",
+        "https://i.pinimg.com/1200x/c8/8b/03/c88b03ab8df06781b8cde0d7378fa5bc.jpg",
       likes: 900,
       post: 28,
       views: 19000,
@@ -95,13 +95,23 @@ const App = () => {
     },
   ];
 
-  return <div className="h-screen bg-black text-white p-4 flex gap-4 flex-wrap">
-      {/* <Card/>
-      <Card/> */}
-      {posts.map(function(){
-        return <Card/>
+  return (
+    <div className="h-fit bg-gray-400 text-white p-4 flex gap-4 flex-wrap items-center justify-center">
+      {posts.map(function (elem, idx) {
+        return (
+          <Card
+            key={idx}
+            image={elem.image}
+            name={elem.name}
+            description={elem.description}
+            posts={elem.post}
+            likes={elem.likes}
+            views={elem.views}
+          />
+        );
       })}
-  </div>;
+    </div>
+  );
 };
 
 export default App;
--- 04-props/src/components/Card.jsx
+++ 04-props/src/components/Card.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React from "react";
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div className='h-87.5 bg-amber-100 w-62.5 rounded-2xl'>
-      
-      
+    <div className="h-fit bg-white w-62.5 rounded-2xl p-1.5 relative">
+      <div className="w-full">
+        <img
+          className="rounded-2xl"
+          src="https://i.pinimg.com/736x/0b/81/7b/0b817b06ab120a4b6a768b39a479ab31.jpg"
+          alt="img"
+        />
+      </div>
+      <div className="bg-white h-25 w-25 rounded-full p-1.5 absolute top-26 left-18  shadow-xl/40">
+        <img
+          className="w-full h-full object-cover rounded-full"
+          src={props.image}
+          alt="Iron Man"
+        />
+      </div>
+      <div className="text-black text-center flex flex-col gap-1 mt-13">
+        <h2 className="text-xl font-bold">{props.name}</h2>
+        <p className="text-sm">
+          {props.description}
+        </p>
+      </div>
+      <div className="flex text-black mt-4 gap-8 justify-center rounded-2xl bg-gray-200 shadow-xl/30 py-3 px-4 mb-4 text-center">
+        <div>
+          <h2 className="text-sm font-bold">{props.likes}</h2>
+          <p className="text-sm">Likes</p>
+        </div>
+        <div>
+          <h2 className="text-sm font-bold">{props.posts}</h2>
+          <p className="text-sm">Posts</p>
+        </div>
+        <div>
+          <h2 className="text-sm font-bold">{props.views}</h2>
+          <p className="text-sm">Views</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
