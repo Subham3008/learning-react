202b0133b1837fc3820a89a0f7295052eb8b838d
Subham3008 <subhamsamanta065@gmail.com>
2026-01-19 23:42:27 +0530

    2nd commit

--- 15-backend_day-1/index.js
+++ 15-backend_day-1/index.js
@@ -1,2 +1,8 @@
-const catMe = require('cat-me')
-console.log(catMe())
+// const catMe = require('cat-me')
+// console.log(catMe())
+
+const express = require('express')
+
+const app = express() //server create ho chuka hai
+
+app.listen(3000)
