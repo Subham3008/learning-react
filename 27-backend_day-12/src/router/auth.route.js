c3156e316600f8091733194e075ce6450fe806d2
Subham3008 <subhamsamanta065@gmail.com>
2026-02-14 23:37:28 +0530

    insta clone login api & controller file done

--- 27-backend_day-12/src/router/auth.route.js
+++ 27-backend_day-12/src/router/auth.route.js
@@ -1,55 +1,13 @@
 const express = require('express')
-const userModel = require('../models/user.model')
-const crypto = require('crypto')
-const jwt = require('jsonwebtoken')
+const authController = require('../controllers/auth.controller')
 
 const authRouter = express.Router()
 
-/*--register---*/
-authRouter.post('/register', async (req, res) => {
-  const { username, email, password, profileImage, bio } = req.body
+{/*--register---*/ }
+authRouter.post('/register', authController.registerController)
 
-  const isUserExists = await userModel.findOne({
-    $or: [
-      { username },
-      { email }
-    ]
-  })
-
-  if (isUserExists) {
-    return res.status(409).json({
-      message: "User already exists" + (isUserExists.email == email ? "Email already exists" : "Username already exists")
-    })
-  }
-
-  const hash = crypto.createHash("sha256").update(password).digest("hex")
-
-  const user = await userModel.create({
-    username,
-    email,
-    password: hash,
-    profileImage,
-    bio
-  })
-
-  const token = jwt.sign({
-    id: user._id
-  },
-    process.env.JWT_SECRET, { expiresIn: "1d" })
-
-  res.cookie("token", token)
-
-  res.status(201).json({
-    message: "User registered successfully",
-    user: {
-      username: user.username,
-      email: user.email,
-      profileImage: user.profileImage,
-      bio: user.bio
-    }
-  })
-
-})
+{/*--login--*/ }
+authRouter.post('/login', authController.loginController)
 
 
 module.exports = authRouter
