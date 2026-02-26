57c1995aee9f1fed4f57679af4a8aba6e39f76cc
Subham3008 <subhamsamanta065@gmail.com>
2026-02-26 17:08:35 +0530

    login & registration done on frontend

--- 29-instaClone/Backend/src/app.js
+++ 29-instaClone/Backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 /*require routes */
 const authRouter = require('./router/auth.route')
@@ -10,6 +11,10 @@ const userRouter = require('./router/user.route')
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+  credentials:true,
+  origin:"http://localhost:5173"
+}))
 
 /*using routes */
 app.use("/api/auth", authRouter)
