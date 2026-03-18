76f0a179bf7e0e67ecd14065cbeb49f156530bd6
Subham3008 <subhamsamanta065@gmail.com>
2026-03-18 11:20:08 +0530

    second commit

--- 34-form-handling/src/App.jsx
+++ 34-form-handling/src/App.jsx
@@ -8,18 +8,33 @@ const App = () => {
     number: "",
   });
 
+  let [arr, setArr] = useState([])
+
   let handleChange = (e) => {
     let { name, value } = e.target;
     setFormValue({ ...formValue, [name]: value });
   };
 
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("after on change ->", formValue);
+
+    setArr([...arr, formValue])
+    console.log("arr values-->", arr);
+    
+
+    setFormValue({
+      name: "",
+      email: "",
+      password: "",
+      number: "",
+    });
+  };
+
   return (
     <div className="flex items-center justify-center h-screen">
       <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log("after on change ->", formValue);
-        }}
+        onSubmit={handleSubmit}
         className="flex flex-col p-8 rounded-xl w-110 h-130 justify-between shadow-[0_10px_30px_rgba(0,0,0,0.15),0_4px_10px_rgba(0,0,0,0.2)] bg-gray-100"
       >
         <div className="flex flex-col items-center gap-1">
@@ -29,6 +44,7 @@ const App = () => {
         <div className="flex flex-col gap-2">
           <label>Full Name</label>
           <input
+            value={formValue.name}
             onChange={handleChange}
             name="name"
             className="border border-gray-300 focus:ring-2 focus:ring-gray-400 outline-none rounded-lg px-4 py-2"
@@ -40,6 +56,7 @@ const App = () => {
         <div className="flex flex-col gap-2">
           <label>Email</label>
           <input
+            value={formValue.email}
             onChange={handleChange}
             name="email"
             className="border border-gray-300 focus:ring-2 focus:ring-gray-400 outline-none rounded-lg px-4 py-2"
@@ -51,6 +68,7 @@ const App = () => {
         <div className="flex flex-col gap-2">
           <label className="flex flex-col gap-2">Password</label>
           <input
+            value={formValue.password}
             onChange={handleChange}
             name="password"
             className="border border-gray-300 focus:ring-2 focus:ring-gray-400 outline-none rounded-lg px-4 py-2"
@@ -62,6 +80,7 @@ const App = () => {
         <div className="flex flex-col gap-2">
           <label className="flex flex-col gap-2">Phone number</label>
           <input
+            value={formValue.number}
             onChange={handleChange}
             name="number"
             className="border border-gray-300 focus:ring-2 focus:ring-gray-400 outline-none rounded-lg px-4 py-2"
