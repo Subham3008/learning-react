90f46a2430f7a15cafa65348e7e2a3ad03592f4f
Subham3008 <subhamsamanta065@gmail.com>
2026-05-10 23:53:33 +0530

    first commit

---
+++ 58-08-Authentication/server.js
@@ -0,0 +1,10 @@
+const app = require("./src/app");
+const connectDb = require("./src/config/db");
+require("dotenv").config()
+
+connectDb()
+
+app.listen(process.env.port, () => {
+  console.log(`Server is starting on port ${process.env.port}`);
+
+})
