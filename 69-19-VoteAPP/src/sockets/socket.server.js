9f72382d582e6e62881cc8c19d2f1f110ca889e4
Subham3008 <subhamsamanta065@gmail.com>
2026-06-06 17:56:51 +0530

    feat: create room features

--- 69-19-VoteAPP/src/sockets/socket.server.js
+++ 69-19-VoteAPP/src/sockets/socket.server.js
@@ -14,13 +14,18 @@ export default function initSocket(httpServer) {
   io.on('connection', (socket) => {
     console.log("A user is connected");
 
+    const { room } = socket.handshake.query
+    console.log(`User joined room: ${room}`);
+
+    socket.join(room)
+
     socket.on("vote_yes", () => {
 
       if (sockets.includes(socket.id)) {
         return
       }
       voteCounts.yes += 1
-      io.emit("vote_update", voteCounts)
+      io.to(room).emit("vote_update", voteCounts)
 
       sockets.push(socket.id)
 
