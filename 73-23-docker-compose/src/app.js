7e1921122059906f204ee0bf97c4a5c6b43db786
Subham3008 <subhamsamanta065@gmail.com>
2026-06-26 01:19:30 +0530

    implement docker compose

--- 73-23-docker-compose/src/app.js
+++ 73-23-docker-compose/src/app.js
@@ -1,8 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
+import indexRouter from "./routes/index.route.js"
 
 const app = express()
 app.use(express.json())
+app.use("/api", indexRouter)
 
 export default app
